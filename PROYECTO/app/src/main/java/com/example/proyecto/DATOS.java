9378b34ed85e1484237422970308f7f39804a020
arnaugoba1315 <arnau.gonzalez.barrios@estudiantat.upc.edu>
2024-11-16 18:47:56 +0100

    v1 Android

---
+++ PROYECTO/app/src/main/java/com/example/proyecto/DATOS.java
@@ -0,0 +1,15 @@
+package com.example.proyecto;
+
+
+
+public class DATOS {
+    public String username;
+    public String password;
+    public String mail;
+
+    public DATOS(String username, String password, String mail) {
+        this.username = username;
+        this.password = password;
+        this.mail= mail;
+    }
+}
---
+++ PROYECTO/app/src/main/java/com/example/proyecto/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.proyecto;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity{
+
+    private Window window;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
